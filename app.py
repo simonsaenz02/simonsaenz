9b3960ff878d00538a2d435e6d0f8d3823132f15
simonsaenz02 <simonsaenz02@gmail.com>
2025-08-28 09:59:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
-
+from PIL import Image
 
 st.title("Trulimero Trulichina")
+
+st.header("Por mas que Trulimero este bravo con Bombini no parcha con Tripi Tropi.")
+st.write("Y si que menos con frigo camelo")
+image= Image.open('trulimero.png')
+
+st.image(image, caption='Trulimero')
